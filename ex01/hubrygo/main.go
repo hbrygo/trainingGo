93f4ca7766a2de43bf079e823d43a3618ec8e310
Hugo Brygo <hubrygo@fu-c4-p4.s19.be>
2025-01-22 14:23:32 +0100

    chat mandatory bientot finis, il faut changer le nom de /events pour le sse

--- ex01/hubrygo/main.go
+++ ex01/hubrygo/main.go
@@ -3,26 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
 type Message struct {
 	Author  string `json:"author"`
 	Content string `json:"content"`
+	Time    string `json:"time"`
+	Image   string `json:"imageId"`
+	RoomId  string `json:"roomId"`
 }
 
 type ChatRoom struct {
-	Name     string     `json:"name"`
-	Messages []*Message `json:"messages"`
+	Name         string     `json:"name"`
+	Key          string     `json:"key"`
+	Messages     []*Message `json:"messages"`
+	LastActivity time.Time  `json:"lastActivity"`
 }
 
 var chatRooms = make(map[string]*ChatRoom)
+var clients = make(map[chan *Message]bool)
+var roomClients = make(map[chan *ChatRoom]bool)
+var clientsMutex sync.Mutex
+var chatRoomsMutex sync.Mutex
 
 func getMethod(w http.ResponseWriter, r *http.Request) {
 	pageName := r.URL.Path[1:]
 	if pageName == "" {
-		pageName = "home"
+		pageName = "index"
 	}
 	pageName = "template/" + pageName + ".html"
 	file, error := os.Open(pageName)
@@ -41,9 +53,14 @@ func getMethod(w http.ResponseWriter, r *http.Request) {
 }
 
 func sendAllChatRoom(w http.ResponseWriter, _ *http.Request) {
-	var availableChatRooms []string
-	for roomName := range chatRooms {
-		availableChatRooms = append(availableChatRooms, roomName)
+	type RoomInfo struct {
+		Name string `json:"name"`
+		Key  string `json:"key"`
+	}
+
+	var availableChatRooms []RoomInfo
+	for _, room := range chatRooms {
+		availableChatRooms = append(availableChatRooms, RoomInfo{Name: room.Name, Key: room.Key})
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -53,38 +70,49 @@ func sendAllChatRoom(w http.ResponseWriter, _ *http.Request) {
 func createRoom(w http.ResponseWriter, r *http.Request) {
 	var requestData struct {
 		Name string `json:"name"`
+		Key  string `json:"key"`
 	}
-
-	// Décoder le corps de la requête JSON
 	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
-
 	roomName := requestData.Name
-	if _, exists := chatRooms[roomName]; !exists {
-		chatRooms[roomName] = &ChatRoom{Name: roomName, Messages: []*Message{}}
-		fmt.Printf("Room created: %v\n", roomName)
+	key := requestData.Key
+	if _, exists := chatRooms[key]; !exists {
+		chatRooms[key] = &ChatRoom{
+			Name:         roomName,
+			Key:          key,
+			Messages:     []*Message{},
+			LastActivity: time.Now(),
+		}
+		// fmt.Printf("Room created: %v\n", chatRooms[key])
+		notifyRoomClients(chatRooms[key])
 	}
 	sendAllChatRoom(w, r)
 }
 
 func sendOldMessages(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("sendOldMessages called") // Ajoutez cette ligne pour vérifier si la fonction est appelée
-
+	// fmt.Println("sendOldMessages called")
 	var requestData struct {
-		RoomName string `json:"roomName"`
+		RoomKey string `json:"roomKey"`
 	}
 
-	// Décoder le corps de la requête JSON
-	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+	// Lire le corps de la requête pour le débogage
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+	// fmt.Printf("Request body: %s\n", body)
+
+	if err := json.Unmarshal(body, &requestData); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
-
-	roomName := requestData.RoomName
-	fmt.Printf("roomName: %v\n", roomName)
-	if room, exists := chatRooms[roomName]; exists {
+	// fmt.Printf("Received request for old messages for room: %v\n", requestData)
+	roomKey := requestData.RoomKey
+	// fmt.Printf("roomKey: %v\n", roomKey)
+	if room, exists := chatRooms[roomKey]; exists {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(room.Messages)
 	} else {
@@ -93,76 +121,173 @@ func sendOldMessages(w http.ResponseWriter, r *http.Request) {
 }
 
 func sendMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("sendMessage called\n")
 	var requestData struct {
-		RoomName string `json:"roomName"`
-		Author   string `json:"author"`
-		Content  string `json:"content"`
+		Author  string `json:"author"`
+		Content string `json:"content"`
+		Image   string `json:"imageId"`
+		RoomId  string `json:"roomId"`
 	}
 
-	// Décoder le corps de la requête JSON
-	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("Request body: %s\n", body)
+
+	if err := json.Unmarshal(body, &requestData); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
+	fmt.Printf("Received message: %v\n", requestData)
 
-	// Ajouter des instructions de débogage
-	fmt.Printf("Received message for room: %v\n", requestData.RoomName)
-	fmt.Printf("Author: %v\n", requestData.Author)
-	fmt.Printf("Content: %v\n", requestData.Content)
-
-	roomName := requestData.RoomName
-	if room, exists := chatRooms[roomName]; exists {
-		room.Messages = append(room.Messages, &Message{Author: requestData.Author, Content: requestData.Content})
-		fmt.Printf("Message added: %v to room %v\n", room.Messages[len(room.Messages)-1], roomName)
+	roomKey := requestData.RoomId
+	if room, exists := chatRooms[roomKey]; exists {
+		message := &Message{
+			Author:  requestData.Author,
+			Content: requestData.Content,
+			Time:    time.Now().Format(time.RFC3339),
+			Image:   requestData.Image,
+			RoomId:  roomKey,
+		}
+		room.Messages = append(room.Messages, message)
+		room.LastActivity = time.Now() // Mettre à jour LastActivity
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"status": "Message sent successfully"})
+		notifyClients(message) // Notifier uniquement les clients de cette salle
 	} else {
 		http.Error(w, "Room not found", http.StatusNotFound)
 	}
 }
 
-func postMethod(w http.ResponseWriter, r *http.Request) {
-	pageName := r.URL.Path[1:]
-	fmt.Printf("pageName: %v\n", pageName)
-	switch pageName {
-	case "chatRoom":
-		sendAllChatRoom(w, r)
-		return
-	case "room":
-		createRoom(w, r)
-		return
-	case "joinChatRoom":
-		// TODO
-	case "getOldMessages":
-		fmt.Printf("getOldMessages\n")
-		sendOldMessages(w, r)
-	case "sendMessage":
-		fmt.Printf("sendMessage\n")
-		sendMessage(w, r)
-	default:
-		http.Error(w, "No Page for this", http.StatusMethodNotAllowed)
+func notifyClients(message *Message) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+	for client := range clients {
+		client <- message
 	}
 }
 
-func selectMethod(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("r.Method: ", r.Method, "\n")
-	switch r.Method {
-	case "GET":
-		getMethod(w, r)
-		return
-	case "POST":
-		postMethod(w, r)
-		return
-	default:
-		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+func notifyRoomClients(room *ChatRoom) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+	for client := range roomClients {
+		client <- room
 	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+	messageChan := make(chan *Message)
+	clientsMutex.Lock()
+	clients[messageChan] = true
+	clientsMutex.Unlock()
+	defer func() {
+		clientsMutex.Lock()
+		delete(clients, messageChan)
+		clientsMutex.Unlock()
+		close(messageChan)
+	}()
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	for {
+		select {
+		case message := <-messageChan:
+			data, _ := json.Marshal(message)
+			fmt.Fprintf(w, "data: %s\n\n", data)
+			flusher.Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
+
+func roomSseHandler(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+	roomChan := make(chan *ChatRoom)
+	clientsMutex.Lock()
+	roomClients[roomChan] = true
+	clientsMutex.Unlock()
+	defer func() {
+		clientsMutex.Lock()
+		delete(roomClients, roomChan)
+		clientsMutex.Unlock()
+		close(roomChan)
+	}()
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	for {
+		select {
+		case room := <-roomChan:
+			data, _ := json.Marshal(room)
+			fmt.Fprintf(w, "data: %s\n\n", data)
+			flusher.Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
+
+func clearRoom() {
+	for {
+		fmt.Println("Running clearRoom")
+		time.Sleep(1 * time.Minute)
+		chatRoomsMutex.Lock()
+		for roomKey, room := range chatRooms {
+			if time.Since(room.LastActivity) > 10*time.Minute {
+				delete(chatRooms, roomKey)
+				fmt.Printf("Room %s cleared due to inactivity\n", roomKey)
+			}
+		}
+		chatRoomsMutex.Unlock()
+	}
+}
+
+func joinRoom(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("joinRoom called\n")
+	roomKey := r.URL.Path[len("/rooms/"):]
+	_, exists := chatRooms[roomKey]
+	if !exists {
+		http.Error(w, "Room not found", http.StatusNotFound)
+		return
+	}
+	pageName := "template/chat.html"
+	file, error := os.Open(pageName)
+	if error != nil {
+		fmt.Printf("404 not found\n")
+		file, error = os.Open("template/404.html")
+		if error != nil {
+			os.Exit(1)
+		}
+	}
+	fileInfo, error := file.Stat()
+	if error != nil {
+		os.Exit(1)
+	}
+	http.ServeContent(w, r, fileInfo.Name(), fileInfo.ModTime(), file)
 }
 
 func main() {
-	// Route pour servir les fichiers HTML
-	http.HandleFunc("/", selectMethod)
-
-	// Lancer le serveur
+	go clearRoom()
+	http.HandleFunc("/", getMethod)
+	http.HandleFunc("/events", sseHandler)
+	http.HandleFunc("/roomEvents", roomSseHandler)
+	http.HandleFunc("GET /rooms/{id}", joinRoom)
+	http.HandleFunc("POST /room", createRoom)
+	http.HandleFunc("POST /rooms/{id}/messages", sendMessage)
+	http.HandleFunc("POST /getOldMessages", sendOldMessages)
+	http.HandleFunc("GET /chatRoom", sendAllChatRoom)
 	fmt.Println("Serveur démarré sur : http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
